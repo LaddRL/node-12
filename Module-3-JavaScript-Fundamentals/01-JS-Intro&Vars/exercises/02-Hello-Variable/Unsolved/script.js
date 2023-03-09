235f5eab54f39584d06e8f41396b502a4c1302b3
ladd lindsay <laddrlindsay@gmail.com>
2023-03-09 10:39:46 -0800

    finished exercise 2 on JS-intro

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
@@ -1,1 +1,19 @@
 // Write Your JavaScript Code Here
+var personName;
+var pets;
+var funFact;
+
+
+personName = "Ladd"
+pets = "2"
+funFact = "I can burp on command"
+
+
+
+personName = "Hayden"
+pets = "1"
+funFact = "I love playing video games"
+
+console.log("My name is " + personName)
+console.log("I have " + pets)
+console.log("Fun fact:" + funFact)
