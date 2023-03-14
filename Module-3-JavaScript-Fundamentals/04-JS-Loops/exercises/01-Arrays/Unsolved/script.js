6a9ca144fcf8d6a12cb56ebeea7399f99a95fb89
ladd lindsay <laddrlindsay@gmail.com>
2023-03-14 13:34:45 -0700

    finished arrays exercise

--- Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
@@ -1,1 +1,13 @@
 // WRITE YOUR CODE HERE
+let names = ["Jon", "Bon", "Jovi"];
+console.log("Welcome to the class " + names[0]);
+names[1] = "Ozzy"
+console.log("Welcome to the class " + names[1]);
+
+console.log("Welcome to the class " + names[2]);
+
+if (names[1] != "Bon"){
+    console.log(names[1] + " is in class")
+}
+
+console.log(names.length)
